688cc523f5b8fefe03d8e99bd519553def347932
Jeff Fichtner <jeff.fichtner@gmail.com>
2024-02-23 09:34:59 -0800

    Add jasmine

---
+++ exercises/js/memes.js
@@ -0,0 +1,5 @@
+function isEven() {
+    // ??
+};
+
+module.exports = isEven;
---
+++ spec/memes.spec.js
@@ -0,0 +1,50 @@
+
+const isEven = require('../exercises/js/memes.js');
+
+describe("1", () => {
+    it("is not even", () => {
+      expect(isEven(1)).toBe(false);
+    });
+  });
+
+describe("2", () => {
+    it("is even", () => {
+      expect(isEven(2)).toBe(true);
+    });
+  });
+
+describe("3", () => {
+    it("is not even", () => {
+      expect(isEven(3)).toBe(false);
+    });
+  });
+
+describe("4", () => {
+    it("is even", () => {
+      expect(isEven(4)).toBe(true);
+    });
+  });
+
+describe("5", () => {
+    it("is not even", () => {
+      expect(isEven(5)).toBe(false);
+    });
+  });
+
+describe("6", () => {
+    it("is even", () => {
+      expect(isEven(6)).toBe(true);
+    });
+  });
+
+describe("7", () => {
+    it("is not even", () => {
+      expect(isEven(7)).toBe(false);
+    });
+  });
+
+describe("8", () => {
+    it("is even", () => {
+      expect(isEven(8)).toBe(true);
+    });
+  });
